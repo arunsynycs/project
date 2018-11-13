0ebf0ea51b69a7dd9435826d7d494a8fb08444a6
synycs <synycs@synycss-mac-mini-2.local>
2018-11-13 17:51:24 +0530

    Initial Commit

---
+++ smartvalue-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smartvalue-ios
+//
+//  Created by synycs on 13/11/18.
+//  Copyright © 2018 synycs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
